3d8919ad6e97bc75c87df3ec323565aea5dcf72a
NigarShamil <shamilovanigar5@gmail.com>
2024-01-14 18:49:22 +0400

    estore navbar part is created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Logo from "./VSF.svg";
+import cartIcon from "./Empty Cart.svg"
+import './navbar.css'
+
+export default function Navbar() {
+    return (
+        <React.Fragment>
+            <div className="navbar">
+                <div className="navbar_start">
+                    <a href="#">CLOTHES</a>
+                    <a href="#">SHOES</a>
+                    <a href="#">TECH</a>
+                </div>
+                <div className="middle">
+                    <img src={Logo} alt="logo" title="logo" />
+                </div>
+                <div className="navbar_end">
+                    <select className="currency">
+                        <option value="USD" selected >USD $</option>
+                        <option value="GBP">GBP £</option>
+                        <option value="AUD">AUD A$</option>
+                        <option value="JPY">JPY ¥</option>
+                    </select>
+                    <div className="cart">
+                        <img src={cartIcon} alt="cartIcon" title="cartIcon" />
+                        <div className="cart_count">0</div>
+                    </div>
+
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
